682cc1e268bba5c4bb834fc73ddbcb438cb40312
Valery Murashko <emestie@gmail.com>
2022-01-12 13:16:08 +0300

    updated mac icon

--- assets/icons/create-icons.sh
+++ assets/icons/create-icons.sh
@@ -14,10 +14,10 @@ del /s 128.png
 del /s 256.png
 del /s 512.png
 
-magick convert rocket.png -resize 16x16 app_icon_16.png 
-magick convert rocket.png -resize 32x32 app_icon_32.png 
-magick convert rocket.png -resize 64x64 app_icon_64.png 
-magick convert rocket.png -resize 128x128 app_icon_128.png 
-magick convert rocket.png -resize 256x256 app_icon_256.png 
-magick convert rocket.png -resize 512x512 app_icon_512.png 
-magick convert rocket.png -resize 1024x1024 app_icon_1024.png 
+magick convert rocket-mac.png -resize 16x16 app_icon_16.png 
+magick convert rocket-mac.png -resize 32x32 app_icon_32.png 
+magick convert rocket-mac.png -resize 64x64 app_icon_64.png 
+magick convert rocket-mac.png -resize 128x128 app_icon_128.png 
+magick convert rocket-mac.png -resize 256x256 app_icon_256.png 
+magick convert rocket-mac.png -resize 512x512 app_icon_512.png 
+magick convert rocket-mac.png -resize 1024x1024 app_icon_1024.png 
